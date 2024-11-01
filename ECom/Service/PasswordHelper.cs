2847134bcb83a1d11380cbc5db2246b846dd8df4
Reinr <blackwhitenxt@gmail.com>
2024-11-01 19:58:21 +0700

    gRPC login

--- ECom/Service/PasswordHelper.cs
+++ ECom/Service/PasswordHelper.cs
@@ -1,4 +1,8 @@
-﻿using System.Security.Cryptography;
+﻿using EComBusiness.Entity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace ECom.Service
@@ -18,6 +22,31 @@ namespace ECom.Service
         {
             var enteredPasswordHash = HashPassword(enteredPassword);
             return storedHashedPassword == enteredPasswordHash;
+        }
+
+        public static string GenerateJwtToken(User user, string jwtKey)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserId),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            };
+            //    var key = new RsaSecurityKey(KeyHelper.GetPrivateKey());
+            //    var creds = new SigningCredentials(key, SecurityAlgorithms.RsaSha256);
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "Trang",
+                audience: "Jade",
+                claims: claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
--- ECom/gRPCService/RPCAuthService.cs
+++ ECom/gRPCService/RPCAuthService.cs
@@ -1,24 +1,77 @@
 ﻿using ECom.Proto;
+using ECom.Service;
+using EComBusiness.Entity;
+using EComBusiness.HelperModel;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECom.gRPCService
 {
     public class RPCAuthService : Auth.AuthBase
     {
         EComContext _context;
-        public RPCAuthService(EComContext context)
+        IConfiguration _config;
+        public RPCAuthService(EComContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
-        public override Task<LoginResponse> Login(LoginRequest request, ServerCallContext context)
+        public override async Task<LoginResponse> Login(LoginRequest request, ServerCallContext context)
         {
-            return base.Login(request, context);
+            var response = new LoginResponse
+            {
+                IsOk = false,
+                Msg = string.Empty,
+                Token = string.Empty
+            };
+            var user = await _context.AppUsers.FirstOrDefaultAsync(u => u.Email == request.Email);
+
+            if (user == null || !PasswordHelper.VerifyPassword(request.Password, user.PasswordHash))
+            {
+                response.IsOk = false;
+                response.Msg = "Invalid credentials.";
+            }
+
+            var token = PasswordHelper.GenerateJwtToken(user, _config["JWT:Key"]);
+            response.IsOk = true;
+            response.Token = token;
+            response.Msg = "Login success.";
+            return response;
         }
 
-        public override Task<RegisterResponse> Register(RegisterRequest request, ServerCallContext context)
+        public override async Task<RegisterResponse> Register(RegisterRequest request, ServerCallContext context)
         {
-            return base.Register(request, context);
+            var response = new RegisterResponse
+            {
+                IsOk = false,
+                Msg = string.Empty
+            };
+
+            if (await _context.Users.AnyAsync(u => u.Email == request.Email))
+            {
+                response.IsOk = false;
+                response.Msg = "Email exist.";
+                return response;
+            }
+            var hashedPassword = PasswordHelper.HashPassword(request.Password);
+
+            var user = new User
+            {
+                UserId = Guid.NewGuid().ToString(),
+                Name = request.Name,
+                Email = request.Email,
+                PasswordHash = hashedPassword,
+                Address = request.Address,
+                PhoneNumber = request.PhoneNumber,
+                Role = UserRole.Customer
+            };
+
+            _context.AppUsers.Add(user);
+            await _context.SaveChangesAsync();
+            response.IsOk = true;
+            response.Msg = "Register success.";
+            return response;
         }
     }
 }
